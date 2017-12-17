86d977e6050158095e09710960208839ed999c5f
Thomasfett93 <tcooke410@gmail.com>
2017-12-17 00:03:07 -0800

    Lesson 1

---
+++ CSA Lesson 1/CSA Lesson 1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace CSA_Lesson_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Base: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Power: ");
+            int p = Convert.ToInt32(Console.ReadLine());
+
+            int result = ExpFun(b, p);
+
+            Console.WriteLine(result);
+            Console.ReadKey();
+
+        }
+
+        static int ExpFun(int originalvar, int power)
+        {
+            int var1 = originalvar;
+            for (int i = 0; i < power - 1; ++i)
+            {
+                var1 *= originalvar;
+            }
+
+            return var1;
+        }
+    }
+}
